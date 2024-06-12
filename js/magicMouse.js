6fcd8a4a0e19d0f51c1436c4aca1c056f1643777
Samuel Rodriguez <samuelrodriguez795@gmail.com>
2024-06-12 10:59:41 -0500

    first commit

---
+++ js/magicMouse.js
@@ -0,0 +1,10 @@
+options = {
+    "outerStyle": "disable",
+    "hoverEffect": "pointer-blur",
+    "hoverItemMove": false,
+    "defaultCursor": false,
+    "outerWidth": 30,
+    "outerHeight": 30
+  }
+magicMouse(options);
+              
